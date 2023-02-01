0216f63f75419575ff2245cdafc19ab995d42f17
mriggire <marco.riggirello@cern.ch>
2023-02-01 16:13:17 +0100

    Removed comments (I'm a dumb persond).

--- src/mille.jl
+++ src/mille.jl
@@ -41,21 +41,11 @@ function dergl(z, m, t)
         1  0  0;
         0  0  0
     ]
-    # θx, θy, θz = Rotations.params(m.R)
-
-    # Яx = RotX(-θx)
-    # Яy = RotY(-θy)
-    # Яz = RotZ(-θz)
-
     Я = inv(m.R)
 
     dρ_dx0 = -Я * @SVector [1, 0, 0]
     dρ_dy0 = -Я * @SVector [0, 1, 0]
     dρ_dz0 = -Я * @SVector [0, 0, 1]
-
-    # dρ_dθx = Яz * Яy * Sx * Яx * (hit - m.r0)
-    # dρ_dθy = Яz * Sy * Яy * Яx * (hit - m.r0)
-    # dρ_dθz = Sz * Яz * Яy * Яx * (hit - m.r0)
 
     dρ_dθx = -Я * (Sx * m.R) * Я * (hit - m.r0)
     dρ_dθy = -Я * (Sy * m.R) * Я * (hit - m.r0)
